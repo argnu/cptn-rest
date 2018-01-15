db11c3ef7e7290d1a85fd01ad1251660942f139a
Martin Weingart <martinweingart@gmail.com>
2018-01-14 21:56:57 -0300

    Fix en add_values

--- tasks/add_values.sql
+++ tasks/add_values.sql
@@ -1,4 +1,4 @@
 ﻿INSERT INTO valores_globales (fecha, nombre, descripcion, valor) VALUES (now(), 'inscripcion_matricula', 'Inscripción de Matrícula', 1000);
 INSERT INTO t_estadoboleta(id, valor) VALUES (10, 'Volante de Pago Generado');
 ALTER TABLE solicitud ADD COLUMN numero SERIAL;
-ALTER SEQUENCE solitud_numero_seq RESTART WITH 115372;
+ALTER SEQUENCE solicitud_numero_seq RESTART WITH 115372;
