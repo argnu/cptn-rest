8688933008bd344a6bbd35a6c411fc830fbb6b9d
Andrés <andresin@gmail.com>
2018-07-14 10:58:46 -0300

    se modifica ruta de subida de fotos de solicitudes para aceptar archivos en base64

--- routes/profesionales.js
+++ routes/profesionales.js
@@ -1,11 +1,14 @@
 const utils = require('../utils');
 const fs = require('fs');
 const path = require('path');
-const multer  = require('multer');
+const multer = require('multer');
 const router = require('express').Router();
 const model = require('../model');
 const bodyParser = require('body-parser');
-router.use(bodyParser.json());
+// create application/json parser
+router.use(bodyParser.json({
+  limit: '10mb'
+}));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -21,22 +24,24 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage })
+const upload = multer({
+  storage: storage
+})
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   model.Profesional.getAll(req.query)
     .then(r => res.json(r))
     .catch(e => utils.errorHandler(e, req, res));
 });
 
-router.get('/:id/contactos', function(req, res) {
+router.get('/:id/contactos', function (req, res) {
   model.Contacto.getAll(req.params.id)
     .then(r => res.json(r))
     .catch(e => utils.errorHandler(e, req, res));
 });
 
 
-router.get('/:id/subsidiarios', function(req, res) {
+router.get('/:id/subsidiarios', function (req, res) {
   model.Subsidiario.getAll(req.params.id)
     .then(r => res.json(r))
     .catch(e => utils.errorHandler(e, req, res));
@@ -47,7 +52,10 @@ router.get('/:id/foto', function (req, res) {
     .then(r => {
       let file_path = path.join(__dirname, '..', 'files/fotos', r);
       if (fs.existsSync(file_path)) res.sendFile(file_path);
-      else return Promise.reject({ code: 404, msg: 'El recurso solicitado no existe' });
+      else return Promise.reject({
+        code: 404,
+        msg: 'El recurso solicitado no existe'
+      });
     })
     .catch(e => utils.errorHandler(e, req, res));
 });
@@ -57,7 +65,10 @@ router.get('/:id/firma', function (req, res) {
     .then(r => {
       let file_path = path.join(__dirname, '..', 'files/firmas', r);
       if (fs.existsSync(file_path)) res.sendFile(file_path);
-      else return Promise.reject({ code: 404, msg: 'El recurso solicitado no existe' });
+      else return Promise.reject({
+        code: 404,
+        msg: 'El recurso solicitado no existe'
+      });
     })
     .catch(e => utils.errorHandler(e, req, res));
 });
@@ -68,58 +79,98 @@ router.get('/:id', function (req, res) {
     .catch(e => utils.errorHandler(e, req, res));
 });
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
   model.Profesional.add(req.body)
     .then(r => res.status(201).json(r))
     .catch(e => utils.errorHandler(e, req, res));
 });
 
-router.put('/:id/foto', upload.fields([{ name: 'foto', maxCount: 1 }]), function(req, res) {
-    if (req.body.foto) {
-      model.Profesional.patch(req.params.id, { foto: req.body.foto })
-        .then(id => res.status(200).json({ id }))
+// upload.fields([{
+//   name: 'foto',
+//   maxCount: 1
+// }]),
+
+router.put('/:id/foto', function (req, res) {
+
+  if (req.body.foto && req.body.filename) {
+
+    const foto = req.body.foto.replace(/^data:(.*);base64,/, "");
+    let ext = path.extname(req.body.filename);
+    let name = req.body.filename.replace(ext, '') + '-' + Date.now() + ext;
+
+    const filename = path.join(__dirname, '../files/fotos/', name);
+
+    fs.writeFile(filename, foto, 'base64', function (err) {
+      if (err) {
+        return next(err)
+      }
+
+      model.Profesional.patch(req.params.id, {
+          foto: filename
+        })
+        .then(id => res.status(200).json({
+          id
+        }))
         .catch(e => utils.errorHandler(e, req, res));
-    }
-    else res.status(500).json({ msg: 'Error en el servidor' });
+    })
+
+  } else res.status(500).json({
+    msg: 'Error en el servidor'
+  });
+
 });
 
-router.put('/:id/firma', upload.fields([{ name: 'firma', maxCount: 1 }]), function(req, res) {
-    if (req.body.firma) {
-      model.Profesional.patch(req.params.id, { firma: req.body.firma })
-        .then(id => res.status(200).json({ id }))
-        .catch(e => utils.errorHandler(e, req, res));
-    }
-    else res.status(500).json({ msg: 'Error en el servidor' });
+router.put('/:id/firma', upload.fields([{
+  name: 'firma',
+  maxCount: 1
+}]), function (req, res) {
+  if (req.body.firma) {
+    model.Profesional.patch(req.params.id, {
+        firma: req.body.firma
+      })
+      .then(id => res.status(200).json({
+        id
+      }))
+      .catch(e => utils.errorHandler(e, req, res));
+  } else res.status(500).json({
+    msg: 'Error en el servidor'
+  });
 });
 
 router.put('/:id',
   upload.fields([{
-    name: 'foto', maxCount: 1
+    name: 'foto',
+    maxCount: 1
   }, {
-    name: 'firma', maxCount: 1
+    name: 'firma',
+    maxCount: 1
   }]),
-  function(req, res) {
+  function (req, res) {
     let profesional = JSON.parse(req.body.profesional);
     profesional.operador = req.user.id;
 
     let proms = [];
 
     if (req.body.foto) {
-      proms.push(model.Profesional.patch(req.params.id, { foto: req.body.foto }));
+      proms.push(model.Profesional.patch(req.params.id, {
+        foto: req.body.foto
+      }));
     }
 
     if (req.body.firma) {
-      proms.push(model.Profesional.patch(req.params.id, { firma: req.body.firma }));
-    }    
+      proms.push(model.Profesional.patch(req.params.id, {
+        firma: req.body.firma
+      }));
+    }
 
     proms.push(model.Profesional.edit(req.params.id, profesional))
 
     Promise.all(proms)
-    .then(id => res.status(200).json(profesional))
-    .catch(e => utils.errorHandler(e, req, res));
-});
+      .then(id => res.status(200).json(profesional))
+      .catch(e => utils.errorHandler(e, req, res));
+  });
 
-router.delete('/:id', function(req, res) {
+router.delete('/:id', function (req, res) {
 
 });
 
